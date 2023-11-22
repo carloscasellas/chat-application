d95c8a1e1070ba4ef2f6f33dc8a27d6a8a0dc069
Carlos Casellas Garza <carloscg10@yahoo.com>
2023-11-22 13:24:48 -0600

    Added online user list

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const ws = require('ws')
 const User = require('./models/User');
 
 mongoose.set("strictQuery", false);
@@ -79,4 +80,30 @@ app.post('/register', async (req, res) => {
     }
 });
 
-app.listen(4040);
+const server = app.listen(4040);
+
+const wss = new ws.WebSocketServer({server});
+
+wss.on('connection', (connection, req) => {
+    const cookies = req.headers.cookie;
+    if(cookies) {
+        const tokenCookieString = cookies.split(';').find(str => str.startsWith('token='));
+        if(tokenCookieString) {
+            const token = tokenCookieString.split('=')[1];
+            if(token) {
+                jwt.verify(token, jwtSecret, {}, (err, userData) => {
+                    if(err) throw err;
+                    const {userId, username} = userData;
+                    connection.userId = userId;
+                    connection.username = username;
+                });
+            }
+        }
+    }
+
+    [...wss.clients].forEach(client => {
+        client.send(JSON.stringify({
+           online: [...wss.clients].map(c => ({userId:c.userId, username:c.username})), 
+        }));
+    });
+});
