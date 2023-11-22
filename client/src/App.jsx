dbcf0a58997211f175fcd161df50549bbf2bae87
Carlos Casellas Garza <carloscg10@yahoo.com>
2023-11-21 19:34:28 -0600

    Finished User Registration and Login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
-import Register from "./Register";
+import axios from "axios";
+import Routes from "./Routes";
+import { UserContextProvider } from "./UserContext";
+
 function App() {
+  axios.defaults.baseURL = 'http://localhost:4040';
+  axios.defaults.withCredentials = true;
 
   return (
-    <Register />
+    <UserContextProvider>
+      <Routes />
+    </UserContextProvider>
   )
 }
 
